68da3ac8f5ffb9bc38bf46fdbcb3aafc566cb72f
ncarvalho <nunocarvalhodossantos@gmail.com>
2018-03-12 23:43:05 +0100

    Make button to search post by title

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 from django.utils import timezone
 from .models import Post, Comment
+from .filters import PostFilter
 from django.shortcuts import get_object_or_404
 from .forms import PostForm, CommentForm
 from django.contrib.auth.decorators import login_required
@@ -39,9 +40,8 @@ def signup(request):
 @login_required
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-
-
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    posts_filter = PostFilter(request.GET, queryset=posts)
+    return render(request, 'blog/post_list.html', {'posts': posts_filter})
 
 
 @login_required
