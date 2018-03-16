9934a30d188449cf56e46d1e64d5b4afe724a63e
cypuyana <cypuyana@synergicpartners.com>
2018-03-16 19:43:15 +0100

    Reverse order posts

--- blog/views.py
+++ blog/views.py
@@ -38,7 +38,7 @@ def signup(request):
 
 @login_required
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
 
 
     return render(request, 'blog/post_list.html', {'posts': posts})
