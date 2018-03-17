ea3f31c8d57d601d0a05da5c5be282ab2f9765a2
ncarvalho <nunocarvalhodossantos@gmail.com>
2018-03-17 16:10:47 +0100

    text input and buttons

--- blog/static/js/post_detail.js
+++ blog/static/js/post_detail.js
@@ -11,69 +11,42 @@ $(document).ready(function() {
     });
 });
 
-//insert after is not implemented in js so you can use this
-function insertAfter(referenceNode, newNode) {
-    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+
+function sendMessage(){
+
+
+
+
+        console.log("Send comment")
+        $(".submit-comment-button").detach()
+        $(".comment-text-area").detach()
+        $('.add-new-comment').text("Add New Comment")
+
+
 }
 
+
+// Add new comment
 $(document).ready(function() {
     $('.add-new-comment').click(function() {
-        console.log("Estroy dentyro")
-        //Disable button after one click, if not show new text input for every click
-        $(this).prop('disabled', true);
+        var state = $('.add-new-comment').text()
+        console.log(state)
+        if(state == "Add New Comment"){
+            $('.add-new-comment').text("Cancel Comment")
 
-        /*
-        var parent = document.getElementById('text-box-input')
-        var div = document.createElement('div')
-        div.setAttribute('class', 'input-group mb-3')
-        div.setAttribute('id', 'input-group mb-3')
-        insertAfter(parent, div)
+            var text_area = document.createElement("TEXTAREA");
+            $("<input type='submit' class='submit-comment-button' onclick='sendMessage()' />").appendTo(".like_comment_div");
 
 
-        var parent = document.getElementById('input-group mb-3')
-        var input = document.createElement("input");
-        input.setAttribute('type', 'text');
-        input.setAttribute('class', 'form-control');
-        input.setAttribute('placeholder', "Left your comment here");
+            $(text_area).addClass("comment-text-area");
+            $(text_area).attr("rows", "4").attr("cols", "60")
+            $('.leave_comment_div').append(text_area)
+        } else{
 
-        parent.appendChild(input)
-
-        parent.appendChild(div)
-        */
-
-        var comment_text = document.createElement("textarea")
-        comment_text.setAttribute("id", "text-box-input")
-        var like_comment_div = document.getElementById('like_comment_div')
-        insertAfter(parent, like_comment_div)
-//This span is used to measure the size of the textarea
-//it should have the same font and text with the textarea and should be hidden
-        var span = $('<span>').css('display','inline-block')
-                              .css('word-break','break-all')
-                              .appendTo('#text-box-input')
-        function initSpan(textarea){
-          span.text(textarea.text())
-              .width(textarea.width())
-              .css('font',textarea.css('font'));
+            $('.add-new-comment').text("Add New Comment")
+            $('.comment-text-area').detach()
+            $('.submit-comment-button').detach()
         }
-        $('textarea').on({
-            input: function(){
-               var text = $(this).val();
-               console.log
-               span.text(text);
-               $(this).height(text ? span.height() : '1.1em');
-            },
-            focus: function(){
-               initSpan($(this));
-            },
-            keypress: function(e){
-               //cancel the Enter keystroke, otherwise a new line will be created
-               //This ensures the correct behavior when user types Enter
-               //into an input field
-               if(e.which == 13) e.preventDefault();
-            }
-        });
-
-
 
     });
 });
