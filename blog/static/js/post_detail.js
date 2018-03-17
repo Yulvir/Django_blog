24eba8ea798c074263abe229a6b8beeca3b7549b
ncarvalho <nunocarvalhodossantos@gmail.com>
2018-03-17 13:07:00 +0100

    post list display and input text improvement

--- blog/static/js/post_detail.js
+++ blog/static/js/post_detail.js
@@ -18,33 +18,62 @@ function insertAfter(referenceNode, newNode) {
 
 $(document).ready(function() {
     $('.add-new-comment').click(function() {
-
+        console.log("Estroy dentyro")
         //Disable button after one click, if not show new text input for every click
         $(this).prop('disabled', true);
 
+        /*
         var parent = document.getElementById('text-box-input')
         var div = document.createElement('div')
         div.setAttribute('class', 'input-group mb-3')
         div.setAttribute('id', 'input-group mb-3')
         insertAfter(parent, div)
 
+
         var parent = document.getElementById('input-group mb-3')
         var input = document.createElement("input");
         input.setAttribute('type', 'text');
         input.setAttribute('class', 'form-control');
         input.setAttribute('placeholder', "Left your comment here");
-        input.style.height="50%";
-        input.style.width="50%";
+
         parent.appendChild(input)
 
-        var div = document.createElement('span')
-        div.setAttribute('class', 'input-group-append')
-        var btn = document.createElement('button')
-        btn.setAttribute('class', 'btn btn-outline-secondary')
-        btn.setAttribute('content', 'Publish');
-        div.appendChild(btn)
-
         parent.appendChild(div)
+        */
+
+        var comment_text = document.createElement("textarea")
+        comment_text.setAttribute("id", "text-box-input")
+        var like_comment_div = document.getElementById('like_comment_div')
+        insertAfter(parent, like_comment_div)
+//This span is used to measure the size of the textarea
+//it should have the same font and text with the textarea and should be hidden
+        var span = $('<span>').css('display','inline-block')
+                              .css('word-break','break-all')
+                              .appendTo('#text-box-input')
+        function initSpan(textarea){
+          span.text(textarea.text())
+              .width(textarea.width())
+              .css('font',textarea.css('font'));
+        }
+        $('textarea').on({
+            input: function(){
+               var text = $(this).val();
+               console.log
+               span.text(text);
+               $(this).height(text ? span.height() : '1.1em');
+            },
+            focus: function(){
+               initSpan($(this));
+            },
+            keypress: function(e){
+               //cancel the Enter keystroke, otherwise a new line will be created
+               //This ensures the correct behavior when user types Enter
+               //into an input field
+               if(e.which == 13) e.preventDefault();
+            }
+        });
+
+
 
     });
 });
