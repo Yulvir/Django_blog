ea73963577e8bdc6c420fc0587d1c341eddcd79c
ncarvalho <nunocarvalhodossantos@gmail.com>
2018-03-31 15:41:07 +0200

    posgres database

--- settings.py
+++ settings.py
@@ -75,13 +75,17 @@ WSGI_APPLICATION = 'django_girls.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dside',
+        'USER': 'ncarvalho',
+        'PASSWORD': 'melides67',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
