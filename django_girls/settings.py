16ec5f628f8b1a91f04cc9792669b60377d3f0bb
cypuyana <cypuyana@synergicpartners.com>
2018-04-09 11:15:50 +0200

    Remove dbsqlite

--- django_girls/settings.py
+++ django_girls/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'django_girls.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db', # set in docker-compose.yml
+        'PORT': 5432 # default postgres port
     }
 }
 
